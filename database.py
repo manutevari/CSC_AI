3a5a65d4271ff3efa2d450bc5baba9208220489d
manutevari <37774043+manutevari@users.noreply.github.com>
2026-03-04 21:51:21 +0530

    Refactor get_connection to use a variable for connection

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 import psycopg2
 import streamlit as st
 
+
 def get_connection():
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         host=st.secrets["database"]["host"],
         port=st.secrets["database"]["port"],
         dbname=st.secrets["database"]["dbname"],
         user=st.secrets["database"]["user"],
         password=st.secrets["database"]["password"]
     )
+    return conn
