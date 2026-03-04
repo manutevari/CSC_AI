87747ea74ad28b40a54a6bb5cc22d24df0d413fc
manutevari <37774043+manutevari@users.noreply.github.com>
2026-03-04 22:25:07 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,12 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-def crawl_website(url):
+def crawl(url):
 
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
 
-    text = soup.get_text()
-
-    return text
+    return soup.get_text()
