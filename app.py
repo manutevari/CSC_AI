a92a443a0fe330e2b5a3ba1df59694fcdd402952
manutevari <37774043+manutevari@users.noreply.github.com>
2026-03-04 22:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
 import streamlit as st
-from database import get_connection
 from mas_engine import ask_ai
 
-st.set_page_config(page_title="CSC Enterprise AI Assistant")
-
 st.title("CSC Enterprise AI Assistant")
-
-# database test
-try:
-    conn = get_connection()
-    st.success("Database connected successfully")
-except Exception as e:
-    st.error("Database connection failed")
-    st.write(e)
 
 query = st.text_input("Ask your question")
 
